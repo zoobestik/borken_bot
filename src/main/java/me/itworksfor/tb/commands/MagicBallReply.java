9eae9346aad948cf2b9bf577fec213bc35899c36
zoobestik <kb.chernenko@gmail.com>
2016-10-09 13:53:58 +0300

    MagicBall; remove unnecessary "no" answer

--- src/main/java/me/itworksfor/tb/commands/MagicBallReply.java
+++ src/main/java/me/itworksfor/tb/commands/MagicBallReply.java
@@ -34,7 +34,6 @@ public class MagicBallReply extends ReplayCommand {
                     "Знаки говорят - нет",
                     "Перспективы не очень хорошие",
                     "Весьма сомнительно",
-                    "Нет",
             };
         }
 
